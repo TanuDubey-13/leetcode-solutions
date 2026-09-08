57fc95ea97e68bb8844e12bf1b8d3d4b2770289a
TANU DUBEY <2k24.cs1q.2413911@gmail.com>
2026-09-08 18:49:30 +0530

    Time: 0 ms (100%), Space: 8.6 MB (51.38%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int countCommas(int n) {
-        int i=0;
+       
         int count=0;
     
-        for(i=1000;i<=n;i++){
+        for(int i=1000;i<=n;i++){
          count++;
         }
         return count;
