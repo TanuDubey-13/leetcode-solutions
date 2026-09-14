a6ba252c22ea6b190ffc39746d41a74f0948d605
TANU DUBEY <2k24.cs1q.2413911@gmail.com>
2026-09-14 14:56:38 +0530

    Time: 2 ms (17.95%), Space: 9.6 MB (6.5%) - LeetHub

---
+++ 3945-digit-frequency-score/3945-digit-frequency-score.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int digitFrequencyScore(int n) {
+        unordered_map<int, int> mpp;
+        vector<int> v;
+        int original=n;
+        while(n!=0){
+            int dig=n%10;
+            v.push_back(dig);
+            n/=10;
+        }
+        for(int x: v){
+            mpp[x]++;
+        }
+        int sum=0;
+        for(auto it : mpp){
+          sum+=it.first*it.second;
+        }
+        return sum;
+    }
+};
