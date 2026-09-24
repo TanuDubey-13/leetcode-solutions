f0206a68b612c0d9caf91300afcaa9b58ca98c84
TANU DUBEY <2k24.cs1q.2413911@gmail.com>
2026-09-24 19:31:39 +0530

    Time: 0 ms (100%), Space: 31.2 MB (63.24%) - LeetHub

---
+++ 3978-unique-middle-element/3978-unique-middle-element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isMiddleElementUnique(vector<int>& nums) {
+        int mid=nums.size()/2;
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+        if(nums[i]==nums[mid]) count++;
+        }
+        if(count>1) return false;
+
+        return true;
+
+    }
+};
